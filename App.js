3f316e7eb777467b3982c4593cd99f3e5fd5df2c
ikegami5 <important2nd@gmail.com>
2021-01-04 07:32:32 +0900

    update gradle dependency

--- App.js
+++ App.js
@@ -1,174 +1,208 @@
 import React from 'react';
 import {
-    StyleSheet,
-    Keyboard,
-    SafeAreaView,
-    ScrollView,
-    View,
-    Text,
-    TextInput,
-    Button,
-    StatusBar,
-    TouchableWithoutFeedback,
+  StyleSheet,
+  Keyboard,
+  SafeAreaView,
+  View,
+  Text,
+  TextInput,
+  Button,
+  StatusBar,
+  TouchableWithoutFeedback,
 } from 'react-native';
-import {
-    Picker,
-} from '@react-native-community/picker';
+import {Picker} from '@react-native-community/picker';
 import Clipboard from '@react-native-community/clipboard';
-import {
-    read_file,
-    get_password,
-} from './util.js'
+import {read_file, get_password} from './util.js';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      records: [],
+      selected_service: 0,
+      selected_salt: 0,
+      password_info: '',
+      master_password: '',
+      timer: null,
+    };
+    this.handleServiceChange.bind(this);
+    this.handleSaltChange.bind(this);
+    this.handleChangeMasterPassword.bind(this);
+    this.handleGeneratePassword.bind(this);
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            records: [],
-            selected_service: 0,
-            selected_salt: 0,
-            password_info: '',
-            master_password: '',
-            timer: null,
-        };
-        this.handleServiceChange.bind(this);
-        this.handleSaltChange.bind(this);
-        this.handleChangeMasterPassword.bind(this);
-        this.handleGeneratePassword.bind(this);
-    }
+  async componentDidMount() {
+    await read_file().then((records) => {
+      this.setState({
+        records: records,
+      });
+    });
+  }
 
-    async componentDidMount() {
-        const records = await read_file();
-        this.setState({
-            records: records,
-        });
-    }
+  componentWillUnmount() {
+    Clipboard.setString('');
+  }
 
-    componentWillUnmount() {
-        Clipboard.setString('');
-    }
+  handleServiceChange(value) {
+    this.setState({
+      selected_salt: 0,
+      selected_service: value,
+    });
+  }
 
-    handleServiceChange(value) {
-        this.setState({
-            selected_salt: 0,
-            selected_service: value,
-        });
-    }
+  handleSaltChange(value) {
+    this.setState({
+      selected_salt: value,
+    });
+  }
 
-    handleSaltChange(value) {
-        this.setState({
-            selected_salt: value,
-        });
-    }
+  handleChangeMasterPassword(value) {
+    this.setState({
+      master_password: value,
+    });
+  }
 
-    handleChangeMasterPassword(value) {
-        this.setState({
-            master_password: value,
-        });
+  handleGeneratePassword() {
+    if (this.state.timer) {
+      clearTimeout(this.state.timer);
     }
+    const master_password = this.state.master_password;
+    const record = this.state.records[this.state.selected_service];
+    const salt = record.salts[this.state.selected_salt];
+    const password_length = record.password_length;
+    const has_capital = record.has_capital;
+    const has_numeral = record.has_numeral;
+    const symbols = record.symbols;
+    const password = get_password(
+      salt,
+      master_password,
+      password_length,
+      has_capital,
+      has_numeral,
+      symbols,
+    );
+    Clipboard.setString(password);
+    const timer = setTimeout(() => {
+      Clipboard.setString('');
+      this.setState({
+        password_info: 'パスワードがクリップボードから消去されました。',
+        timer: null,
+      });
+    }, 30000);
+    this.setState({
+      master_password: '',
+      timer: timer,
+      password_info:
+        'パスワードがクリップボードにコピーされました。\n30秒経過すると消去されます。',
+    });
+    this.masterPasswordInput.blur();
+  }
 
-    handleGeneratePassword() {
-        if (this.state.timer) {
-            clearTimeout(this.state.timer);
-        }
-        const master_password = this.state.master_password;
-        const record = this.state.records[this.state.selected_service];
-        const salt = record.salts[this.state.selected_salt];
-        const password_length = record.password_length;
-        const has_capital = record.has_capital;
-        const has_numeral = record.has_numeral;
-        const symbols = record.symbols;
-        const password = get_password(salt, master_password, password_length, has_capital, has_numeral, symbols);
-        Clipboard.setString(password);
-        const timer = setTimeout(() => {
-            Clipboard.setString('');
-            this.setState({
-                password_info: 'パスワードがクリップボードから消去されました。',
-                timer: null,
-            });
-        }, 30000);
-        this.setState({
-            master_password: '',
-            timer: timer,
-            password_info: 'パスワードがクリップボードにコピーされました。\n30秒経過すると消去されます。',
-        });
-        this.masterPasswordInput.blur();
-    }
+  handleClearPassword() {
+    Clipboard.setString('');
+  }
 
-    handleClearPassword() {
-        Clipboard.setString('');
-    }
-
-    render() {
-        return (
-            <>
-                <StatusBar barStyle={'dark-content'} />
-                <SafeAreaView style={styles.base}>
-                    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false} style={styles.base}>
-                        <View style={styles.content} keyboardShouldPersistTaps={'never'}>
-                            <Picker selectedValue={this.state.selected_service}
-                                onValueChange={(value) => this.handleServiceChange(value)}>
-                                {
-                                    this.state.records.map((r, i) => {
-                                        return <Picker.Item label={r.service_name} value={i} key={i} />;
-                                    })
-                                }
-                            </Picker>
-                            <Text style={styles.serviceInfo} selectable={true}>
-                                {
-                                    this.state.records[this.state.selected_service] ?
-                                        this.state.records[this.state.selected_service].service_info : ''
-                                }
-                            </Text>
-                            {
-                                this.state.records[this.state.selected_service] ?
-                                <Picker selectedValue={this.state.selected_salt} style={styles.picker}
-                                    onValueChange={(value) => this.handleSaltChange(value)}>
-                                    {
-                                        this.state.records[this.state.selected_service].salts.map((s, i) => {
-                                            return <Picker.Item label={i ? i + 'つ前のパスワード' : '現在のパスワード'} value={i} key={i} />;
-                                        })
-                                    }
-                                </Picker> : <Text></Text>
-                            }
-                            <TextInput secureTextEntry={true} autoCompleteType={'off'} autoCorrect={false}
-                                placeholder={'マスターパスワード'} style={styles.masterPasswordInput}
-                                value={this.state.master_password} ref={(input) => {this.masterPasswordInput = input}}
-                                onChangeText={(value) => this.handleChangeMasterPassword(value)}></TextInput>
-                            <Button title={'生成'} onPress={() => this.handleGeneratePassword()} />
-                            <Text style={styles.passwordInfo}>{this.state.password_info}</Text>
-                            <Button title={'パスワード消去（アプリを終了する前に必ず押して下さい。）'} onPress={() => this.handleClearPassword()} />
-                        </View>
-                    </TouchableWithoutFeedback>
-                </SafeAreaView>
-            </>
-        );
-    }
-};
+  render() {
+    return (
+      <>
+        <StatusBar barStyle={'dark-content'} />
+        <SafeAreaView style={styles.base}>
+          <TouchableWithoutFeedback
+            onPress={Keyboard.dismiss}
+            accessible={false}
+            style={styles.base}>
+            <View style={styles.content} keyboardShouldPersistTaps={'never'}>
+              <Picker
+                selectedValue={this.state.selected_service}
+                onValueChange={(value) => this.handleServiceChange(value)}>
+                {this.state.records.map((r, i) => {
+                  return (
+                    <Picker.Item label={r.service_name} value={i} key={i} />
+                  );
+                })}
+              </Picker>
+              <Text style={styles.serviceInfo} selectable={true}>
+                {this.state.records[this.state.selected_service]
+                  ? this.state.records[this.state.selected_service].service_info
+                  : ''}
+              </Text>
+              {this.state.records[this.state.selected_service] ? (
+                <Picker
+                  selectedValue={this.state.selected_salt}
+                  style={styles.picker}
+                  onValueChange={(value) => this.handleSaltChange(value)}>
+                  {this.state.records[this.state.selected_service].salts.map(
+                    (s, i) => {
+                      return (
+                        <Picker.Item
+                          label={
+                            i ? i + 'つ前のパスワード' : '現在のパスワード'
+                          }
+                          value={i}
+                          key={i}
+                        />
+                      );
+                    },
+                  )}
+                </Picker>
+              ) : (
+                <Text />
+              )}
+              <TextInput
+                secureTextEntry={true}
+                autoCompleteType={'off'}
+                autoCorrect={false}
+                placeholder={'マスターパスワード'}
+                style={styles.masterPasswordInput}
+                value={this.state.master_password}
+                ref={(input) => {
+                  this.masterPasswordInput = input;
+                }}
+                onChangeText={(value) => this.handleChangeMasterPassword(value)}
+              />
+              <Button
+                title={'生成'}
+                onPress={() => this.handleGeneratePassword()}
+              />
+              <Text style={styles.passwordInfo}>
+                {this.state.password_info}
+              </Text>
+              <Button
+                title={
+                  'パスワード消去（アプリを終了する前に必ず押して下さい。）'
+                }
+                onPress={() => this.handleClearPassword()}
+              />
+            </View>
+          </TouchableWithoutFeedback>
+        </SafeAreaView>
+      </>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-    base: {
-        flex: 1,
-    },
-    content: {
-        padding: 5,
-        flex: 1,
-    },
-    picker: {
-        margin: 5,
-    },
-    serviceInfo: {
-        borderWidth: 1,
-    },
-    masterPasswordInput: {
-        borderWidth: 1,
-        padding: 5,
-        margin: 5,
-    },
-    passwordInfo: {
-        color: 'orange',
-    },
+  base: {
+    flex: 1,
+  },
+  content: {
+    padding: 5,
+    flex: 1,
+  },
+  picker: {
+    margin: 5,
+  },
+  serviceInfo: {
+    borderWidth: 1,
+  },
+  masterPasswordInput: {
+    borderWidth: 1,
+    padding: 5,
+    margin: 5,
+  },
+  passwordInfo: {
+    color: 'orange',
+  },
 });
 
 export default App;
--- util.js
+++ util.js
@@ -1,114 +1,141 @@
 import * as RNFS from 'react-native-fs';
 import sha512 from 'crypto-js/sha512';
-import {
-    Random,
-} from './random.js';
-import {
-    Binary,
-} from './binary.js';
+import {Random} from './random.js';
+import {Binary} from './binary.js';
 
 async function read_file() {
-    return await RNFS.readDir(RNFS.ExternalDirectoryPath).then(files => {
-        return files.find(f => f.name === 'data.txt');
-    }).then(file => {
-        return RNFS.readFile(file.path);
-    }).then(content => {
-        return parse_records(content);
-    }).catch(err => {
-        console.log(err.message);
+  return await RNFS.readDir(RNFS.ExternalDirectoryPath)
+    .then((files) => {
+      return files.find((f) => f.name === 'data.txt');
+    })
+    .then((file) => {
+      return RNFS.readFile(file.path);
+    })
+    .then((content) => {
+      return parse_records(content);
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
-    return '';
 }
 
 function parse_records(data) {
-    return data.split(/\r?\n/).map((d) => {
-        return new Record(d.split(',').map((s) => s.trim()));
-    });
+  return data.split(/\r?\n/).map((d) => {
+    return new Record(d.split(',').map((s) => s.trim()));
+  });
 }
 
 class Record {
-
-    constructor(data) {
-        this.service_name = data[0].replace(/\\C/g, ',');
-        this.service_info = data[1].replace(/\\C/g, ',').replace(/\\n/g, '\n');
-        this.has_capital = data[2] === 'true';
-        this.has_numeral = data[3] === 'true';
-        this.symbols = data[4].replace(/\\S/g, ' ').replace(/\\C/g, ',');
-        this.password_length = parseInt(data[5], 10);
-        this.salts = data.slice(6).reverse();
-    }
-
+  constructor(data) {
+    this.service_name = data[0].replace(/\\C/g, ',');
+    this.service_info = data[1].replace(/\\C/g, ',').replace(/\\n/g, '\n');
+    this.has_capital = data[2] === 'true';
+    this.has_numeral = data[3] === 'true';
+    this.symbols = data[4].replace(/\\S/g, ' ').replace(/\\C/g, ',');
+    this.password_length = parseInt(data[5], 10);
+    this.salts = data.slice(6).reverse();
+  }
 }
 
-function get_password(salt, master_password, password_length, has_capital, has_numeral, symbols) {
-    const hashed_string = hash_64_times(master_password, salt);
-    return format_password(hashed_string, password_length, has_capital, has_numeral, symbols);
+function get_password(
+  salt,
+  master_password,
+  password_length,
+  has_capital,
+  has_numeral,
+  symbols,
+) {
+  const hashed_string = hash_64_times(master_password, salt);
+  return format_password(
+    hashed_string,
+    password_length,
+    has_capital,
+    has_numeral,
+    symbols,
+  );
 }
 
-export function format_password(str, password_length, has_capital, has_numeral, symbols) {
-    const initial_seed = parseInt(str.substring(456, 484), 2) + parseInt(str.substring(484), 2);
-    const alphabets = 'abcdefghijklmnopqrstuvwxyz';
-    const capital_alphabets = has_capital ? 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' : '';
-    const numerals = has_numeral ? '0123456789' : '';
-    const letters = alphabets + capital_alphabets + numerals + symbols;
-    const letters_list = Array.prototype.map.call(letters, (l) => l);
-    for (let additional_seed = 0; additional_seed <= 100; additional_seed++) {
-        const random = new Random(initial_seed + additional_seed);
-        const tmp_letters_list = random.shuffle(letters_list);
-        const tmp_password = [...Array(password_length).keys()].map((index) => {
-            const word_length = Math.floor(str.length / password_length);
-            const word_string = str.substring(word_length * index, word_length * index + word_length - 1);
-            const letter_index = new Binary(word_string).mod(new Binary(tmp_letters_list.length.toString(2)));
-            return tmp_letters_list[letter_index];
-        }).join('');
-        const is_proper_password =
-            /^.*[a-z].*$/.test(tmp_password) &&
-            (has_capital ? /^.*[A-Z].*$/.test(tmp_password) : true) &&
-            (has_numeral ? /^.*[0-9].*$/.test(tmp_password) : true) &&
-            (symbols ? new RegExp('^.*[' + symbols.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&') + '].*$').test(tmp_password) : true);
-        if (is_proper_password) {
-            return tmp_password;
-        }
+export function format_password(
+  str,
+  password_length,
+  has_capital,
+  has_numeral,
+  symbols,
+) {
+  const initial_seed =
+    parseInt(str.substring(456, 484), 2) + parseInt(str.substring(484), 2);
+  const alphabets = 'abcdefghijklmnopqrstuvwxyz';
+  const capital_alphabets = has_capital ? 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' : '';
+  const numerals = has_numeral ? '0123456789' : '';
+  const letters = alphabets + capital_alphabets + numerals + symbols;
+  const letters_list = Array.prototype.map.call(letters, (l) => l);
+  for (let additional_seed = 0; additional_seed <= 100; additional_seed++) {
+    const random = new Random(initial_seed + additional_seed);
+    const tmp_letters_list = random.shuffle(letters_list);
+    const tmp_password = [...Array(password_length).keys()]
+      .map((index) => {
+        const word_length = Math.floor(str.length / password_length);
+        const word_string = str.substring(
+          word_length * index,
+          word_length * index + word_length - 1,
+        );
+        const letter_index = new Binary(word_string).mod(
+          new Binary(tmp_letters_list.length.toString(2)),
+        );
+        return tmp_letters_list[letter_index];
+      })
+      .join('');
+    const is_proper_password =
+      /^.*[a-z].*$/.test(tmp_password) &&
+      (has_capital ? /^.*[A-Z].*$/.test(tmp_password) : true) &&
+      (has_numeral ? /^.*[0-9].*$/.test(tmp_password) : true) &&
+      (symbols
+        ? new RegExp(
+            '^.*[' + symbols.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&') + '].*$',
+          ).test(tmp_password)
+        : true);
+    if (is_proper_password) {
+      return tmp_password;
     }
-    console.log('パスワードが生成できなかったよ');
-    return '';
+  }
+  console.log('パスワードが生成できなかったよ');
+  return '';
 }
 
 export function hash(str) {
-    const hex = sha512(str).toString();
-    const binary = Array.prototype.map.call(hex, (c) => {
-        return hexToBinary(c);
-    }).join('');
-    return binary;
+  const hex = sha512(str).toString();
+  const binary = Array.prototype.map
+    .call(hex, (c) => {
+      return hexToBinary(c);
+    })
+    .join('');
+  return binary;
 }
 
 function hexToBinary(hex) {
-    return ('0000' + parseInt(hex, 16).toString(2)).slice(-4);
+  return ('0000' + parseInt(hex, 16).toString(2)).slice(-4);
 }
 
 function hash_twice(str, salt) {
-    return hash(salt + hash(str + salt));
+  return hash(salt + hash(str + salt));
 }
 
 function compose(func) {
-    return (str, salt) => {
-        return func(func(str, salt), salt);
-    };
+  return (str, salt) => {
+    return func(func(str, salt), salt);
+  };
 }
 
 function hash_8_times(str, salt) {
-    return compose(compose(hash_twice))(str, salt);
+  return compose(compose(hash_twice))(str, salt);
 }
 
 function hash_32_times(str, salt) {
-    return compose(compose(hash_8_times))(str, salt);
+  return compose(compose(hash_8_times))(str, salt);
 }
 
 function hash_64_times(str, salt) {
-    return compose(hash_32_times)(str, salt);
+  return compose(hash_32_times)(str, salt);
 }
 
-export {
-    read_file,
-    get_password,
-};
+export {read_file, get_password};
